c0bad22ba8ff1a33b099ecbcc65a59fff16cf027
Tomoaki Teshima <tomoaki.teshima@gmail.com>
2015-01-14 10:56:21 +0000

    add comparison of diff  - not only absdiff but compare the performance of just diff  - cut out the comparison code as a function  - remove Japanese comment  - cut out the output of measurement time as function

--- main.cpp
+++ main.cpp
@@ -4,11 +4,119 @@
 #include <opencv2/imgcodecs/imgcodecs.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/cudaarithm.hpp>
+#include <opencv2/cudafilters.hpp>
 #include <iostream>
+
+const char LENA[] = "lena.jpg";
+const char IDOJUN[] = "compare.jpg";
+const char defaultUnit[] = "[ms]";
+
+void dumpConsumedTime(int64 countStart, int64 countStop, const char* unit = NULL)
+{
+	if(unit == NULL)
+	{
+		unit = defaultUnit;
+	}
+	std::cout << ((countStop - countStart) * 1000) / cv::getTickFrequency() << unit << std::endl;
+}
+
+#if 0
+void comapareSobel(const char* filename)
+{
+	using namespace cv;
+	Mat input;
+
+	input = imread(filename);
+
+	Mat result;
+	int64 countStart = getTickCount();
+	cv::blur(input, result, 3);
+	int64 countStop = getTickCount();
+
+	dumpConsumedTime(countStart, countStop, defaultUnit);
+
+	int64 countBeforeTransfer = getTickCount();
+	cuda::GpuMat gpuInput = cuda::GpuMat(input);
+	cuda::GpuMat gpuResult;
+	cuda::createGaussianFilter(gpuInput.type, gpuInput.type, 3);
+
+	countStart = getTickCount();
+	cuda::Filter::apply(input, result, 
+	cuda::subtract(gpuBefore, gpuAfter, gpuDiff);
+	countStop = getTickCount();
+
+	dumpConsumedTime(countStart, countStop, defaultUnit);
+	dumpConsumedTime(countBeforeTransfer, countStop, "[ms] (including transfer)");
+}
+#endif
+
+void compareDiff(const char* filenameBefore, const char* filenameAfter)
+{
+	using namespace cv;
+	Mat before;
+	Mat after;
+
+	before = imread(filenameBefore);
+	after = imread(filenameAfter);
+
+	Mat diff;
+	int64 countStart = getTickCount();
+	diff = before - after;
+	int64 countStop = getTickCount();
+
+	dumpConsumedTime(countStart, countStop, defaultUnit);
+
+	int64 countBeforeTransfer = getTickCount();
+	cuda::GpuMat gpuBefore = cuda::GpuMat(before);
+	cuda::GpuMat gpuAfter  = cuda::GpuMat(after);
+	cuda::GpuMat gpuDiff;
+
+	countStart = getTickCount();
+	cuda::subtract(gpuBefore, gpuAfter, gpuDiff);
+	countStop = getTickCount();
+
+	dumpConsumedTime(countStart, countStop, defaultUnit);
+	dumpConsumedTime(countBeforeTransfer, countStop, "[ms] (including transfer)");
+}
+
+void compareAbsDiff(const char* filenameBefore, const char* filenameAfter)
+{
+	using namespace cv;
+	Mat before;
+	Mat after;
+
+	before = imread(filenameBefore);
+	after = imread(filenameAfter);
+
+	Mat diff;
+	int64 countStart = getTickCount();
+	for(int i = 0;i < 10;i++)
+	{
+		absdiff(before, after, diff);
+	}
+	int64 countStop = getTickCount();
+
+	dumpConsumedTime(countStart, countStop, defaultUnit);
+
+	int64 countBeforeTransfer = getTickCount();
+	cuda::GpuMat gpuBefore = cuda::GpuMat(before);
+	cuda::GpuMat gpuAfter  = cuda::GpuMat(after);
+	cuda::GpuMat gpuDiff;
+
+	countStart = getTickCount();
+	for(int i = 0;i < 10;i++)
+	{
+		cuda::absdiff(gpuBefore, gpuAfter, gpuDiff);
+	}
+	countStop = getTickCount();
+
+	dumpConsumedTime(countStart, countStop, defaultUnit);
+	dumpConsumedTime(countBeforeTransfer, countStop, "[ms] (including transfer)");
+}
 
 int main(int argc, const char* argv[])
 {
-	// ƒrƒ‹ƒhî•ñ‚ð•\Ž¦‚·‚é
+	// dump the build information
 	std::cout << cv::getBuildInformation() << std::endl;
 
 	std::cout << "CPU_MMX   : " << cv::checkHardwareSupport(CV_CPU_MMX   ) << std::endl;
@@ -57,7 +165,7 @@ int main(int argc, const char* argv[])
 		std::cout << "vendor [" << iDevice << "]             : " << ocl_platform_info[iDevice].vendor() << std::endl;
 		std::cout << "version[" << iDevice << "]             : " << ocl_platform_info[iDevice].version() << std::endl;
 		cv::ocl::Device hoge;
-	    ocl_platform_info[iDevice].getDevice(hoge, iDevice);
+		ocl_platform_info[iDevice].getDevice(hoge, iDevice);
 		std::cout << "name            [" << iDevice << "]    : " << hoge.name() << std::endl;
 		std::cout << "extensions      [" << iDevice << "]    : " << hoge.extensions() <<  std::endl;
 		std::cout << "version         [" << iDevice << "]    : " << hoge.version()    <<  std::endl;
@@ -69,30 +177,8 @@ int main(int argc, const char* argv[])
 		std::cout << "Driver Version  [" << iDevice << "]    : " << hoge.driverVersion() << std::endl;
 	}
 
-	cv::Mat before;
-	cv::Mat after;
-
-	before = cv::imread("lena.jpg");
-	after = cv::imread("lena.jpg");
-
-	cv::Mat diff;
-	int64 countStart = cv::getTickCount();
-	cv::absdiff(before, after, diff);
-	int64 countStop = cv::getTickCount();
-
-	std::cout << ((countStop - countStart) * 1000) / cv::getTickFrequency() << "[ms]" << std::endl;
-
-	int64 countBeforeTransfer = cv::getTickCount();
-	cv::cuda::GpuMat gpuBefore = cv::cuda::GpuMat(before);
-	cv::cuda::GpuMat gpuAfter  = cv::cuda::GpuMat(after);
-	cv::cuda::GpuMat gpuDiff;
-
-	countStart = cv::getTickCount();
-	cv::cuda::absdiff(gpuBefore, gpuAfter, gpuDiff);
-	countStop = cv::getTickCount();
-
-	std::cout << ((countStop - countStart) * 1000) / cv::getTickFrequency() << "[ms]" << std::endl;
-	std::cout << ((countStop - countBeforeTransfer) * 1000) / cv::getTickFrequency() << "[ms] (including transfer)" << std::endl;
+	compareAbsDiff(LENA, IDOJUN);
+	compareDiff(LENA, IDOJUN);
 
 	return 0;
 }
